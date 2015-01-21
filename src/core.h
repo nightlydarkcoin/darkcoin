09857e112448aa3566c0e7a2b92c04cd696c63a1
Holger Schinzel <holger.schinzel@gmx.de>
2015-01-21 07:45:27 +0100

    changed ping to 15min

--- src/core.h
+++ src/core.h
@@ -30,7 +30,7 @@ static const int64_t DARKSEND_POOL_MAX = (999.99*COIN);
 #define MASTERNODE_REMOTELY_ENABLED            9
 
 #define MASTERNODE_MIN_CONFIRMATIONS           15
-#define MASTERNODE_MIN_DSEEP_SECONDS           (30*60)
+#define MASTERNODE_MIN_DSEEP_SECONDS           (15*60)
 #define MASTERNODE_MIN_DSEE_SECONDS            (5*60)
 #define MASTERNODE_PING_SECONDS                (1*60)
 #define MASTERNODE_EXPIRATION_SECONDS          (65*60)
